333234f970806ea8f02dc6f1829ce26478d81495
artiwarhade <117365356+artiwarhade@users.noreply.github.com>
2022-11-17 17:28:38 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Q1 
+{
+  public static void main(String [] args)
+
+
+{ 
+ 
+ArrayList<String> list_strings = new ArrayList<String>();
+
+list_strings.add("Pink");
+     
+list_strings.add("Green");
+     
+list_strings.add("Orange");
+     
+list_strings.add("White");
+     
+list_strings.add("Black");
+
+System.out.println(list_strings);
+     
+}
+}
